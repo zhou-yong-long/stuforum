9fde94122c8a06f676e1e10db367213f6807c496
zhou-yong-long <zhouyonglong09@126.com>
2020-11-20 11:32:28 +0800

    逆向工程生成dao、entity、mapper

---
+++ src/main/java/dao/ArticlePictureMapper.java
@@ -0,0 +1,21 @@
+package dao;
+
+import entity.ArticlePicture;
+import entity.ArticlePictureExample;
+import java.util.List;
+
+public interface ArticlePictureMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(ArticlePicture record);
+
+    int insertSelective(ArticlePicture record);
+
+    List<ArticlePicture> selectByExample(ArticlePictureExample example);
+
+    ArticlePicture selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(ArticlePicture record);
+
+    int updateByPrimaryKey(ArticlePicture record);
+}
